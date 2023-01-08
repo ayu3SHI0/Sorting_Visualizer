3f1096735b0d1c9956c145e22f8593482420cf84
Ayushi Chauhan <78093967+ayu3SHI0@users.noreply.github.com>
2023-01-08 19:51:53 +0530

    Add files via upload

---
+++ scripts/selection_sort.js
@@ -0,0 +1,51 @@
+async function selection(){
+    //Time complexities
+    document.getElementById("Time_Worst").innerText="O(N^2)";
+    document.getElementById("Time_Average").innerText="Θ(N^2)";
+    document.getElementById("Time_Best").innerText="Ω(N^2)";
+
+    //Space complexity
+    document.getElementById("Space_Worst").innerText="O(1)";
+
+    console.log('In selection()');
+    const ele = document.querySelectorAll(".bar");
+    for(let i = 0; i < ele.length; i++){
+        console.log('In ith loop');
+        let min_index = i;
+        // Changing color of the position to swap with the next min
+        ele[i].style.background = 'blue';
+        for(let j = i+1; j < ele.length; j++){
+            console.log('In jth loop');
+            ele[j].style.background = 'red';
+
+            await waitforme(delay);
+            if(parseInt(ele[j].style.height) < parseInt(ele[min_index].style.height)){
+                console.log('In if condition height comparision');
+                if(min_index !== i){
+                    ele[min_index].style.background = 'cyan';
+                }
+                min_index = j;
+            } 
+            else{
+                ele[j].style.background = 'cyan';
+            }   
+        }
+        await waitforme(delay);
+        swap(ele[min_index], ele[i]);
+        // change the min element index back to normal as it is swapped 
+        ele[min_index].style.background = 'cyan';
+        // change the sorted elements color to green
+        ele[i].style.background = 'green';
+    }
+}
+
+const selectionSortbtn = document.querySelector(".selectionSort");
+selectionSortbtn.addEventListener('click', async function(){
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    await selection();
+    enableSortingBtn();
+    enableSizeSlider();
+    enableNewArrayBtn();
+});
